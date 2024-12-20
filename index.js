1ab3d3b114a58d1675d5c988e4aaa7ca48a5d70a
Syed Muhammad Omar Afnan <138956919+omar-afnan@users.noreply.github.com>
2024-12-20 12:30:14 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 let count = 0 
@@ -13,3 +15,20 @@ function save() {
     countEl.textContent = 0
     count = 0
 }
+
+// Decrement button
+const decrementBtn = document.getElementById("decrement-btn"); 
+// Reset button
+const resetBtn = document.getElementById("reset-btn"); 
+
+// Assign function to decrement button
+decrementBtn.onclick = function () {
+    count--;
+    countEl.textContent = count;
+};
+
+// Assign function to reset button
+resetBtn.onclick = function () {
+    count = 0;
+    countEl.textContent = count;
+};
